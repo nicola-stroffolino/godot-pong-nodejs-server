89cd281f510db9431e9821bacc73ce92e3265a26
Nicola Stroffolino <91049774+nicola-stroffolino@users.noreply.github.com>
2024-05-12 23:44:18 +0200

    deck and card draw

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const WebSocket = require("ws");
+const fs = require('fs');
 
 const port = 5000;
 const wss = new WebSocket.Server({ port: port });
+const defaultDeck = JSON.parse(fs.readFileSync('deck.json', 'utf8'));
 
 let rooms = [];
 
 console.log(`running on port: ${port}`);
 
 wss.on("connection", (socket) => {
-  let player = null;
-
   socket.on("message", (message) => {
     let data = JSON.parse(message);
 
@@ -27,6 +27,18 @@ wss.on("connection", (socket) => {
         let roomToJoin = getRoomByName(data.Data.Name);
         if (roomToJoin && roomToJoin.password === data.Data.Password) {
           socket = joinRoom(roomToJoin, socket, data.Data.Nickname);
+          var socket_2 = roomToJoin.players.find((player) => player.id != socket.id);
+
+          socket.send(JSON.stringify({ oppNickname: socket_2.nickname }));
+          socket_2.send(JSON.stringify({ oppNickname: socket.nickname }));
+          
+          roomToJoin.deck = shuffleDeck(roomToJoin.deck);
+          
+          socket.cards = drawCards(roomToJoin.deck, 7);
+          socket_2.cards = drawCards(roomToJoin.deck, 7);
+          
+          socket.send(JSON.stringify({ draw: socket.cards }));
+          socket_2.send(JSON.stringify({ draw: socket_2.cards }));
         } else {
           socket.send(JSON.stringify({ error: "Room does not exist or incorrect password" }));
         }
@@ -37,9 +49,12 @@ wss.on("connection", (socket) => {
         room.players.forEach(player => {
           if (player.id != playerData.id) {
             console.log('player ' + player.id + ' is receiving data.');
-            player.send(JSON.stringify({ id: player.id, x: playerData.x, y: playerData.y }))
+            player.send(JSON.stringify({ id: player.id, x: playerData.x, y: playerData.y }));
           }
         });
+        break;
+      case "Play Card":
+
         break;
       default:
         break;
@@ -54,7 +69,7 @@ wss.on("connection", (socket) => {
         rooms = rooms.filter((r) => r !== room);
       }
     }
-    console.log(`Player ${socket.nickname} disconnected`);
+    console.log(`Player ${socket.nickname} - Room ${room.name} disconnected`);
   });
 });
 
@@ -63,6 +78,7 @@ function createRoom(name, password) {
     name: name,
     password: password,
     players: [],
+    deck: defaultDeck.deck
   };
 }
 
@@ -79,4 +95,16 @@ function joinRoom(room, socket, nickname) {
 
 function getRoomByName(name) {
   return rooms.find((room) => room.name === name);
+}
+
+function shuffleDeck(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Scambio gli elementi
+  }
+  return array;
+}
+
+function drawCards(array, count) {
+  return array.splice(-count, count);
 }
